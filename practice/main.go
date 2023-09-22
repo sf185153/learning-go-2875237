7dccafaacf037b41e0bfb13755a3ea03dc0b7958
Stephen Farmer <sf185153@ncr.com>
2023-09-22 10:46:18 -0400

    1

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	n := time.Now()
+	fmt.Println("Dates and times, ", n)
 
+	t := time.Date(2009, time.April, 23, 4, 1, 50, 0, time.UTC)
+	fmt.Println("made date: ", t)
+
+	parseTime, _ := time.Parse(time.ANSIC, "Tue Nov 10 23:00:00 2009")
+
+	fmt.Printf(parseTime.String())
 }

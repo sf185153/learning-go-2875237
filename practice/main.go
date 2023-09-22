487df10846eeaadc4c0d7fddccd9ae6691c80085
Stephen Farmer <sf185153@ncr.com>
2023-09-22 13:11:48 -0400

    1

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	var p = &anInt // p is a pointer, pointing at nil
+
+	fmt.Println("P value, ", *p)
+	*p = *p + 1
+	fmt.Println("P value, ", *p)
+	fmt.Println("Int is, ", anInt)
 }

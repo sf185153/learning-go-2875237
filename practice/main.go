72555fa1a04946255d5483b64dce1c57e8e0233a
Stephen Farmer <sf185153@ncr.com>
2023-09-25 09:22:21 -0400

    1

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Conditional logic")
+	ans := 42
+	var result string
+
+	if ans < 0 {
+		result = "Less zero"
+	} else if ans == 0 {
+		result = "0"
+	} else {
+		result = "bigger than zero"
+	}
+	fmt.Println(result)
+
+	if x := -42; x < 0 {
+		result = "Less zero"
+	} else if x == 0 {
+		result = "0"
+	} else {
+		result = "bigger than zero"
+	}
+	fmt.Println(result)
 }

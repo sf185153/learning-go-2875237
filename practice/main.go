f8e927dd8329a91eaf7eada1b0972de067103bc3
Stephen Farmer <sf185153@ncr.com>
2023-09-25 13:51:07 -0400

    1

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("retype: %T", resp)
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	content := string(bytes)
+	fmt.Println(content)
 }

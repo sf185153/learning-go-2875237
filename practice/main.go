292bcb7e41d49bbd74e805dfa4cb1bd4744690f9
Stephen Farmer <sf185153@ncr.com>
2023-09-25 10:33:14 -0400

    1

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+	sim := addVals(5, 8)
+	fmt.Println(sim)
+	fmt.Println(addAllVals(1, 2, 3, 4))
+}
+
+func doSomething() {
+	fmt.Println("wowzers")
+}
+
+func addVals(val1 int, val2 int) int {
+	return val1 + val2
+}
+
+func addAllVals(values ...int) (int, int) {
+	total := 0
+	for _, i := range values {
+		total += i
+	}
+	return total, len(values)
 }

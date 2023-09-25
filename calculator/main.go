a5c96d087b9cc8b2702c73ad69b2e302831baa14
Stephen Farmer <sf185153@ncr.com>
2023-09-25 11:28:25 -0400

    1

--- calculator/main.go
+++ calculator/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("Files")
+	content := "hello"
+	file, err := os.Create("./fromString.txt")
+	checkErr(err)
+	length, err := io.WriteString(file, content)
+	checkErr(err)
+	fmt.Printf("wrote a file with %v", length)
+	defer file.Close()
+	defer readFile("./fromString.txt")
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(fileName string) {
+	data, err := os.ReadFile(fileName)
+	checkErr(err)
+	fmt.Printf("text read %v, ", string(data))
 }
